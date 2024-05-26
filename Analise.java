5e00ad555742b55927f6c64039323c10591c1900
Celsovicente <celsovicente222@gmail.com>
2024-05-26 15:18:24 +0100

    Primeiro comite

---
+++ Analise.java
@@ -0,0 +1,53 @@
+/*------------------------------------
+ Tema: Gestão de
+ Número: 33019
+ Ficheiro: Analise.java
+ Data: 23/05/2024
+------------------------------------ */
+
+/*------------------------------------
+    1 - Objetivo 
+    Este projeto tem como objetivo fazer o registro da inscrição e do pagamento de um curso 
+------------------------------------*/
+
+/*-----------------------------------
+    2 - Visão 
+    ApresentacaoVisao
+    LoginVisao
+    MenuPrincipal
+    FormandoVisao
+    InscricaoVisao
+    PagamentoVisao
+*/
+
+/*------------------------------------
+    3 - Entidades Fortes e Seus Atributos (Modelo)
+    FormandoModelo
+    int id
+    String nome
+    String dataNascimento
+    char genero
+    String contacto
+
+    InscricaoModelo
+    String bilhete
+    float valor
+
+    PagamentoModelo 
+    String id
+---------------------------------*/
+
+/*-------------------------------
+    4 - Ficheiro
+    FormandoFile.dat
+    InscricaoFile.dat
+    PagamentoFile.dat
+----------------------------------*/
+
+/*----------------------------------
+    5 - Ficheiro
+    Formador.tab
+    Curso.tab
+    Nacionalidade.tab
+    Morada.tab (provincia, municipio, comuna)
+-----------------------------------*/
